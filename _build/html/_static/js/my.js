2e177d0678fe33f8a6a3f99c5a411715871c3e80
bSploit <bSploit@gmail.com>
2020-03-27 20:28:58 +0300

    analytics after compile

--- _build/html/_static/js/my.js
+++ _build/html/_static/js/my.js
@@ -1,8 +1,8 @@
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
+
    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   (window, document, "script", "https://ipmeta.io/plugin.js", "ipmeta");
 
    ym(57652708, "init", {
         clickmap:true,
@@ -10,6 +10,12 @@
         accurateTrackBounce:true,
         webvisor:true
    });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/57652708" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
+
+  ga('require', 'ipMeta', {
+      serviceProvider: 'dimension1',
+      networkDomain: 'dimension2',
+      networkType: 'dimension3',
+  });
+  ga('ipMeta:loadNetworkFields');
+
+  
