528b5247f53ceb3b940161e1fe6a199b7c0227ec
bSploit <bSploit@gmail.com>
2020-03-27 20:45:49 +0300

    analytics

--- _build/html/_static/js/my.js
+++ _build/html/_static/js/my.js
@@ -2,14 +2,18 @@
    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-   (window, document, "script", "https://ipmeta.io/plugin.js", "ipmeta");
-
+   
    ym(57652708, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
    });
+
+(function(i,s,o,g,r,a,m){i['ipmeta']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://ipmeta.io/plugin.js','ipmeta');
 
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
