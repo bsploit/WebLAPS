604a53c41d04c4417f4a05d3de303750feeedecd
bSploit <bSploit@gmail.com>
2020-03-27 20:37:22 +0300

    analytics

--- _static/js/my.js
+++ _static/js/my.js
@@ -11,11 +11,19 @@
         webvisor:true
    });
 
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-121627672-1', 'auto');
   ga('require', 'ipMeta', {
       serviceProvider: 'dimension1',
       networkDomain: 'dimension2',
       networkType: 'dimension3',
   });
   ga('ipMeta:loadNetworkFields');
+  ga('send', 'pageview');
 
   
